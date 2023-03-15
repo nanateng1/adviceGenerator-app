469a7a1693ff9631c3d1a67af1c6397365c00658
nanateng1 <nanateng2020@gmail.com>
2023-03-15 20:24:45 +0000

    complete challenge basics

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', getFetch)
+
+function getFetch() {
+    const URL = 'https://api.adviceslip.com/advice';
+
+    fetch(URL)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            document.getElementById('adviceID').innerText = `ADVICE #${data.slip.id}`
+
+            document.getElementById('adviceMsg').innerText = `"${data.slip.advice}"`
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
